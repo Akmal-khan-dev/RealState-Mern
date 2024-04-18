99964f601795abaf91bc00dab18cb2483c689e69
Akmal khan <akmalkh58@gmail.com>
2024-04-18 13:52:08 +0500

    UI Home page created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,41 @@
-import Navbar from './components/navbar/Navbar.jsx'
-import './layout.scss'
+
+import HomePage from './pages/Home/HomePage.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Layout from './pages/Layout/Layout.jsx';
+import ListPage from './pages/List/ListPage.jsx';
+import SinglePage from './pages/Single/SinglePage.jsx';
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element: <Layout/>,
+      children:[
+        {
+          path:'/',
+          element: <HomePage/>
+        },
+        {
+          path:'/list',
+          element:<ListPage/>
+        },
+        {
+          path:'/:id',
+          element:<SinglePage/>
+        }
+      ]
+    },
+    
+  ])
   return (
-    <div className="layout">
-        <Navbar/>
-    </div>
+    
+    <RouterProvider router={router} />
   )
 }
 
---
+++ client/src/pages/Single/SinglePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './singlepage.scss';
+
+const SinglePage = () => {
+   
+  return (
+    <div>single page</div>
+  )
+}
+
+export default SinglePage
